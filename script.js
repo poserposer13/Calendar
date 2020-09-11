8c43f68646eb3ed14a95d6f934c838120ef27a4a
Sean Kempf <poserposer@me.com>
2020-09-10 22:45:41 -0700

    Created script.js, started some code that will cover the start

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+
+
+let today = Date.now(new Date());
+currentDay = today
+console.log(currentDay)
+
+ 
+let time;
+time = 
+let newRow = $('<tr>');
+let hourCell = $('<td>');
+hourCell.text(time);
+newRow.append(hourCell)
+
+
+
+});
