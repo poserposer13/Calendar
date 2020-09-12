3f3625fac71da610123a7708bcba5d7f1711fd2f
Sean Kempf <poserposer@me.com>
2020-09-12 14:15:55 -0700

    Updated the visuals to be in dark mode

--- script.js
+++ script.js
@@ -2,24 +2,15 @@ $(document).ready(function () {
 
 
 
-    // let d = new Date();
-
-    // let month = d.getMonth() + 1;
-    // let day = d.getDate();
-
-    // let output =
-    //     (month < 10 ? '0' : '') + month + '/' +
-    //     (day < 10 ? '0' : '') + day + '/' +
-    //     d.getFullYear();
-    let output = moment();
-    $('#currentDay').text(output);
+    let currentDay = moment().format("dddd, MMMM Do YYYY");
+    $('#currentDay').text(currentDay);
 
 
     let timeArr = ['9:00AM', '10:00AM', '11:00AM', '12:00AM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM'];
     for (let i = 0; i < timeArr.length; i++) {
         let time = timeArr[i];
 
-        let newTable = $('<table>').addClass("table");
+        let newTable = $('<table>').addClass("table rounded");
         let hourCell = $('<td>').addClass('col-sm-4');
         hourCell.text(time);
         newTable.append(hourCell);
@@ -34,7 +25,7 @@ $(document).ready(function () {
         input.val(savedInput);
 
         let saveEl = $('<td>').addClass('col-sm-3');
-        let saveBtn = $('<button>').addClass('save-btn');
+        let saveBtn = $('<button>').addClass('btn btn-info');
         saveBtn.attr('date-time');
         saveBtn.text('save');
         saveEl.append(saveBtn);
