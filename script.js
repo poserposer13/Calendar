f92a540ee763abe72ccc1c95f28317c9c8394614
Sean Kempf <poserposer@me.com>
2020-09-12 13:59:33 -0700

    Changed the div into a table

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ $(document).ready(function () {
 
 
 
-    let d = new Date();
+    // let d = new Date();
 
-    let month = d.getMonth() + 1;
-    let day = d.getDate();
+    // let month = d.getMonth() + 1;
+    // let day = d.getDate();
 
-    let output =
-        (month < 10 ? '0' : '') + month + '/' +
-        (day < 10 ? '0' : '') + day + '/' +
-        d.getFullYear();
+    // let output =
+    //     (month < 10 ? '0' : '') + month + '/' +
+    //     (day < 10 ? '0' : '') + day + '/' +
+    //     d.getFullYear();
+    let output = moment();
     $('#currentDay').text(output);
 
 
@@ -18,32 +19,39 @@ $(document).ready(function () {
     for (let i = 0; i < timeArr.length; i++) {
         let time = timeArr[i];
 
-        let newRow = $('<div>').addClass("row");
-        let hourCell = $('<div>').addClass('col-sm-4');
+        let newTable = $('<table>').addClass("table");
+        let hourCell = $('<td>').addClass('col-sm-4');
         hourCell.text(time);
-        newRow.append(hourCell);
+        newTable.append(hourCell);
 
-        let inputEl = $('<div>').addClass('col-sm-5');
+        let inputEl = $('<td>').addClass('col-sm-5');
         let input = $('<input type="text">');
         input.addClass('input');
         input.attr('id', time);
         inputEl.append(input);
-        newRow.append(inputEl);
+        newTable.append(inputEl);
         let savedInput = localStorage.getItem(time);
         input.val(savedInput);
 
-        let saveEl = $('<div>').addClass('col-sm-3');
+        let saveEl = $('<td>').addClass('col-sm-3');
         let saveBtn = $('<button>').addClass('save-btn');
         saveBtn.attr('date-time');
         saveBtn.text('save');
         saveEl.append(saveBtn);
-        newRow.append(saveEl);
+        newTable.append(saveEl);
 
-        $('.container-fluid').append(newRow);
+        $('.container-fluid').append(newTable);
 
         saveBtn.on('click', function (){
             localStorage.setItem(time, input.val());
-        })
+        });
+
+        let hourArr = [9,10,11,12,13,14,15,16,17]
+        let currentHour = moment().hour();
+        if (currentHour === hourArr[i]){
+            newTable.addClass('bg-danger')
+        }
+       
     }
 
 
